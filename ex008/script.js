19d83189483336fc4953b394e8a7bf31db7dfc6f
Alan Teixeira <86982421+Alanteixeira29@users.noreply.github.com>
2023-02-27 11:39:02 -0300

    Add files via upload

---
+++ ex008/script.js
@@ -0,0 +1,10 @@
+var vel = 60.5
+
+document.write(`A velocidade de seu carro é de <strong style="color: #00b48d;">${vel}Km/h.<strong> <br>`)
+if (vel > 60) {
+    document.write('Você ultrapassou a velocidade permitida, consequentemente você foi <strong style="color: red;">MULTADO! </strong>')
+}
+
+else {
+document.write("Dirija sempre com o cinto de segurança!")
+}
